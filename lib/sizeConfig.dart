7240e25732fe551db49d8465a4d4b9a5e3e7b1db
Amal Anan <133678416+amalanan@users.noreply.github.com>
2025-07-25 20:22:10 +0300

    Create sizeConfig.dart

---
+++ lib/sizeConfig.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static late MediaQueryData _mediaQueryData;
+  static late double screenWidth;
+  static late double screenHeight;
+  static late double blockWidth;
+  static late double blockHeight;
+
+  static void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+
+    // كل 1% من الشاشة
+    blockWidth = screenWidth / 100;
+    blockHeight = screenHeight / 100;
+  }
+
+  // لتحديد حجم الخط بنسبة من العرض
+  static double responsiveText(double percentage) {
+    return blockWidth * percentage;
+  }
+}
